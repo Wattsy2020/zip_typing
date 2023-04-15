911e7bc15604e16c7fc23607a2fab53d0408ec65
Liam Watts <liam.watts271@gmail.com>
2023-04-15 18:55:40 +0800

    tuple_zip: support single elem tuple case

--- test_tuple_zip.py
+++ test_tuple_zip.py
@@ -1,7 +1,15 @@
 from tuple_zip import zip_
 
 
-def test_two_tuples() -> None:
+def test_tuple_one_elem() -> None:
+    tuple1 = (1,)
+    tuple2 = (2,)
+    zipped = zip_(tuple1, tuple2)
+    assert zipped == ((1, 2),)
+    assert zipped == tuple(zip(tuple1, tuple2))
+
+
+def test_tuple_two_elem() -> None:
     tuple1 = (1, "a")
     tuple2 = (2, "b")
     zipped = zip_(tuple1, tuple2)
--- tuple_zip.py
+++ tuple_zip.py
@@ -1,16 +1,24 @@
 """Implements a Zip that types the Iterable[tuple[_t1, _t2, _t3]] -> tuple[list[_t1], list[_t2], list[_t3]] case correctly"""
-from typing import TypeVar
+from typing import TypeVar, overload
 
 _T1 = TypeVar("_T1")
 _T2 = TypeVar("_T2")
 
 
-# To solve this we need to write the generics differently
-def zip_(*iterables: tuple[_T1, _T2]) -> tuple[tuple[_T1, ...], tuple[_T2, ...]]:
-    tuples = list(iterables)
-    list1: list[_T1] = []
-    list2: list[_T2] = []
-    for elem1, elem2 in tuples:
-        list1.append(elem1)
-        list2.append(elem2)
-    return tuple(list1), tuple(list2)
+@overload
+def zip_(*tuples: tuple[_T1]) -> tuple[tuple[_T1, ...]]:
+    ...
+
+
+@overload
+def zip_(*tuples: tuple[_T1, _T2]) -> tuple[tuple[_T1, ...], tuple[_T2, ...]]:
+    ...
+
+
+def zip_(*tuples: tuple) -> tuple[tuple, ...]:
+    num_elems = len(tuples[0])
+    outputs: tuple[list, ...] = tuple(list() for _ in range(num_elems))
+    for elems in tuples:
+        for i, elem in enumerate(elems):
+            outputs[i].append(elem)
+    return tuple(tuple(output) for output in outputs)
